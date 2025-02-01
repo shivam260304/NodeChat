7100dd2fd63ad0d4e8e56e179286b51da424f158
shivam260304 <shivam260304@gmail.com>
2025-02-01 20:47:06 +0530

    socket.io initialized 3:45 done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,33 +1,42 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import { connectToDb } from './lib/db.js';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
+
+import { connectToDb } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
 dotenv.config();
 
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-const port = process.env.PORT;
-const app = express ();
-
-// This will help express to extract the json data out of req.body
 app.use(express.json());
-app.use(express.urlencoded({extended: true })); 
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
-}))
+  })
+);
 
-app.get('/', (req,res) =>{
-    res.send("This is home page")
-})
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.listen(port, () =>{
-    console.log(`Server is running on port ${port}`);
-    connectToDb();
-})
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectToDb();
+});
